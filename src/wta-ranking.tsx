c494578f248a3cbd8b1abce39380fbcf4b4d0c4b
drillprop <drylbartosz@gmail.com>
2024-02-12 22:13:44 +0100

    add metadata

--- src/wta-ranking.tsx
+++ src/wta-ranking.tsx
@@ -44,6 +44,41 @@ const WtaRanking = () => {
             title={`${player.ranking}. ${getCountryEmoji(player.country)} ${player.name}`}
             subtitle={player.country}
             accessories={getAccesories(player)}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="Age" text={player.age.toString()} />
+                    <List.Item.Detail.Metadata.Label title="Country" text={player.country} />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label title="Current ranking" text={player.ranking.toString()} />
+                    <List.Item.Detail.Metadata.Label
+                      title="Ranking change from previous release."
+                      text={player.rankingChange?.toString() || "0"}
+                    />
+                    <List.Item.Detail.Metadata.Label title="Points" text={player.points?.toString()} />
+                    <List.Item.Detail.Metadata.Label
+                      title="Points change from previous release"
+                      text={player.pointsChange?.toString() || "0"}
+                    />
+                    <List.Item.Detail.Metadata.Label title="Country rank" text={player.countryRank.toString()} />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label
+                      title="Current tournament"
+                      text={player.currentTournament || "-"}
+                    />
+                    <List.Item.Detail.Metadata.Label
+                      title="Next points in tournament"
+                      text={player.next?.toString() || "-"}
+                    />
+                    <List.Item.Detail.Metadata.Label
+                      title="Max points in tournament"
+                      text={player.max?.toString() || "-"}
+                    />
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
           />
         ))}
       </List.Section>
