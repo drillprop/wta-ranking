ab760a9d5e0d224e91d80c10652f7600edf6c25c
Bartosz Dryl <drylbartosz@gmail.com>
2024-02-22 12:42:07 +0100

    Feat/color rank values (#8)
    * use tags for ranking values
    
    * add more colors, replace country with ranking change

--- src/wta-ranking.tsx
+++ src/wta-ranking.tsx
@@ -1,7 +1,9 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, Color, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useState } from "react";
 import { getCountryInfo } from "./utils/getCountryInfo";
+import { ListTag } from "./components/ListTag";
+import { getSignedNumberNotationInString } from "./utils/getSignedNumberNotation";
 
 type Player = {
 	ranking: number;
@@ -22,11 +24,31 @@ const WtaRanking = () => {
 	const { data: players, isLoading } = useFetch<Player[]>("http://127.0.0.1:8080/live-ranking");
 	const [showDetails, setShowDetails] = useState(false);
 
+	const getRankingColor = (num: number) => {
+		if (num === 0) {
+			return Color.SecondaryText;
+		}
+		if (num > 0) {
+			return Color.Green;
+		}
+		if (num < 0) {
+			return Color.Red;
+		}
+	};
+
 	const getAccesories = (player: Player): List.Item.Accessory[] | null | undefined => {
 		if (showDetails) {
 			return null;
 		}
-		return [{ text: `Points: ${player.points.toString()}` }];
+		return [
+			{
+				tag: {
+					value: getSignedNumberNotationInString(player.pointsChange || 0),
+					color: getRankingColor(player.pointsChange || 0),
+				},
+			},
+			{ text: `Points: ${player.points.toString()}` },
+		];
 	};
 
 	return (
@@ -44,28 +66,40 @@ const WtaRanking = () => {
 						}
 						key={idx}
 						title={`${player.ranking}. ${getCountryInfo(player.country).emoji} ${player.name}`}
-						subtitle={player.country}
+						subtitle={player.rankingChange ? getSignedNumberNotationInString(player.rankingChange || 0) : undefined}
 						accessories={getAccesories(player)}
 						detail={
 							<List.Item.Detail
 								metadata={
 									<List.Item.Detail.Metadata>
-										<List.Item.Detail.Metadata.Label title={player.name} />
-										<List.Item.Detail.Metadata.Label title="Age" text={player.age.toString()} />
-										<List.Item.Detail.Metadata.Label title="Country" text={getCountryInfo(player.country).name} />
-										<List.Item.Detail.Metadata.Separator />
-										<List.Item.Detail.Metadata.Label title="Current ranking" text={player.ranking.toString()} />
-										<List.Item.Detail.Metadata.Label title="Career high" text={player.careerHigh.toString()} />
-										<List.Item.Detail.Metadata.Label title="Country rank" text={player.countryRank.toString()} />
-										<List.Item.Detail.Metadata.Separator />
 										<List.Item.Detail.Metadata.Label
-											title="Ranking change from previous release"
-											text={player.rankingChange?.toString() || "0"}
+											title={player.name}
+											text={`${getCountryInfo(player.country).name} ${getCountryInfo(player.country).emoji}`}
 										/>
+										<List.Item.Detail.Metadata.Label title="Age" text={player.age.toString()} />
 										<List.Item.Detail.Metadata.Label title="Points" text={player.points?.toString()} />
-										<List.Item.Detail.Metadata.Label
+										<List.Item.Detail.Metadata.Separator />
+										<ListTag title="Current ranking" text={player.ranking.toString()} color={Color.Blue} />
+										<ListTag
+											title="Career high"
+											text={player.careerHigh.toString()}
+											color={player.ranking === player.careerHigh ? Color.Yellow : Color.SecondaryText}
+										/>
+										<ListTag
+											title="Country rank"
+											text={player.countryRank.toString()}
+											color={player.countryRank === 1 ? Color.Yellow : Color.SecondaryText}
+										/>
+										<List.Item.Detail.Metadata.Separator />
+										<ListTag
+											title="Ranking change from previous release"
+											text={getSignedNumberNotationInString(player.rankingChange || 0)}
+											color={getRankingColor(player.rankingChange || 0)}
+										/>
+										<ListTag
 											title="Points change from previous release"
-											text={player.pointsChange?.toString() || "0"}
+											text={getSignedNumberNotationInString(player.pointsChange || 0)}
+											color={getRankingColor(player.pointsChange || 0)}
 										/>
 										<List.Item.Detail.Metadata.Separator />
 										<List.Item.Detail.Metadata.Label
