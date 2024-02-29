042a7ca64b6bdc92c2d62377fa8ef5c3b8e4a41c
drillprop <drylbartosz@gmail.com>
2024-02-29 21:10:10 +0100

    add show player in ranking action

--- src/wta-ranking.tsx
+++ src/wta-ranking.tsx
@@ -25,6 +25,7 @@ const WtaRanking = () => {
 	const { data: players, isLoading } = useFetch<Player[]>("http://127.0.0.1:8080/live-ranking");
 	const [showDetails, setShowDetails] = useState(false);
 	const [searchInput, setSearchInput] = useState<string>();
+	const [selectedPlayerId, setSelectedPlayerId] = useState<string>();
 
 	const getRankingColor = (num: number) => {
 		if (num === 0) {
@@ -68,77 +69,89 @@ const WtaRanking = () => {
 			navigationTitle="Live wta ranking"
 			isLoading={isLoading}
 			searchBarPlaceholder="Search player"
+			selectedItemId={selectedPlayerId}
 			isShowingDetail={showDetails}
 			filtering={false}
 			onSearchTextChange={setSearchInput}
 		>
 			<List.Section title="Live WTA ranking">
-				{filteredPlayers?.map((player, idx) => (
-					<List.Item
-						actions={
-							<ActionPanel>
-								<Action
-									title={showDetails ? "Hide Details" : "Show Details"}
-									onAction={() => setShowDetails((x) => !x)}
+				{filteredPlayers?.map((player, idx) => {
+					const playerId = player.ranking + player.name;
+					return (
+						<List.Item
+							id={playerId}
+							actions={
+								<ActionPanel>
+									<Action
+										title={showDetails ? "Hide Details" : "Show Details"}
+										onAction={() => setShowDetails((x) => !x)}
+									/>
+									<Action
+										title="Show Player in Ranking"
+										onAction={() => {
+											setSelectedPlayerId(playerId);
+											setSearchInput(undefined);
+										}}
+									/>
+								</ActionPanel>
+							}
+							key={idx}
+							title={`${player.ranking}. ${getCountryInfo(player.country).emoji} ${player.name}`}
+							subtitle={player.rankingChange ? getSignedNumberNotationInString(player.rankingChange || 0) : undefined}
+							accessories={getAccesories(player)}
+							detail={
+								<List.Item.Detail
+									metadata={
+										<List.Item.Detail.Metadata>
+											<List.Item.Detail.Metadata.Label
+												title={player.name}
+												text={`${getCountryInfo(player.country).name} ${getCountryInfo(player.country).emoji}`}
+											/>
+											<List.Item.Detail.Metadata.Label title="Age" text={player.age.toString()} />
+											<List.Item.Detail.Metadata.Label title="Points" text={player.points?.toString()} />
+											<List.Item.Detail.Metadata.Separator />
+											<ListTag title="Current ranking" text={player.ranking.toString()} color={Color.SecondaryText} />
+											<ListTag
+												title="Career high"
+												text={player.careerHigh.toString()}
+												color={player.ranking === player.careerHigh ? Color.Yellow : Color.SecondaryText}
+											/>
+											<ListTag
+												title="Country rank"
+												text={player.countryRank.toString()}
+												color={player.countryRank === 1 ? Color.Yellow : Color.SecondaryText}
+											/>
+											<List.Item.Detail.Metadata.Separator />
+											<ListTag
+												title="Ranking change from previous release"
+												text={getSignedNumberNotationInString(player.rankingChange || 0)}
+												color={getRankingColor(player.rankingChange || 0)}
+											/>
+											<ListTag
+												title="Points change from previous release"
+												text={getSignedNumberNotationInString(player.pointsChange || 0)}
+												color={getRankingColor(player.pointsChange || 0)}
+											/>
+											<List.Item.Detail.Metadata.Separator />
+											<List.Item.Detail.Metadata.Label
+												title="Current tournament"
+												text={player.currentTournament || "-"}
+											/>
+											<List.Item.Detail.Metadata.Label
+												title="Next points(if player wins next match)"
+												text={player.next?.toString() || "-"}
+											/>
+											<List.Item.Detail.Metadata.Label
+												title="Max points(if player wins tournament)"
+												text={player.max?.toString() || "-"}
+											/>
+										</List.Item.Detail.Metadata>
+									}
 								/>
-							</ActionPanel>
-						}
-						key={idx}
-						title={`${player.ranking}. ${getCountryInfo(player.country).emoji} ${player.name}`}
-						subtitle={player.rankingChange ? getSignedNumberNotationInString(player.rankingChange || 0) : undefined}
-						accessories={getAccesories(player)}
-						detail={
-							<List.Item.Detail
-								metadata={
-									<List.Item.Detail.Metadata>
-										<List.Item.Detail.Metadata.Label
-											title={player.name}
-											text={`${getCountryInfo(player.country).name} ${getCountryInfo(player.country).emoji}`}
-										/>
-										<List.Item.Detail.Metadata.Label title="Age" text={player.age.toString()} />
-										<List.Item.Detail.Metadata.Label title="Points" text={player.points?.toString()} />
-										<List.Item.Detail.Metadata.Separator />
-										<ListTag title="Current ranking" text={player.ranking.toString()} color={Color.SecondaryText} />
-										<ListTag
-											title="Career high"
-											text={player.careerHigh.toString()}
-											color={player.ranking === player.careerHigh ? Color.Yellow : Color.SecondaryText}
-										/>
-										<ListTag
-											title="Country rank"
-											text={player.countryRank.toString()}
-											color={player.countryRank === 1 ? Color.Yellow : Color.SecondaryText}
-										/>
-										<List.Item.Detail.Metadata.Separator />
-										<ListTag
-											title="Ranking change from previous release"
-											text={getSignedNumberNotationInString(player.rankingChange || 0)}
-											color={getRankingColor(player.rankingChange || 0)}
-										/>
-										<ListTag
-											title="Points change from previous release"
-											text={getSignedNumberNotationInString(player.pointsChange || 0)}
-											color={getRankingColor(player.pointsChange || 0)}
-										/>
-										<List.Item.Detail.Metadata.Separator />
-										<List.Item.Detail.Metadata.Label
-											title="Current tournament"
-											text={player.currentTournament || "-"}
-										/>
-										<List.Item.Detail.Metadata.Label
-											title="Next points(if player wins next match)"
-											text={player.next?.toString() || "-"}
-										/>
-										<List.Item.Detail.Metadata.Label
-											title="Max points(if player wins tournament)"
-											text={player.max?.toString() || "-"}
-										/>
-									</List.Item.Detail.Metadata>
-								}
-							/>
-						}
-					/>
-				))}
+							}
+						/>
+					);
+				})}
 			</List.Section>
 		</List>
 	);
